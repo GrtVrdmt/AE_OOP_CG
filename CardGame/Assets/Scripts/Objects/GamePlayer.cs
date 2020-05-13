5f1d2d2df0afa74a2dcd48140caf321e9b37a728
GrtVrdmt <mgwobrock@outlook.de>
2020-05-13 14:06:53 +0200

    Included Playerclass and Script

---
+++ CardGame/Assets/Scripts/Objects/GamePlayer.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Ressources;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlayer : MonoBehaviour
+{
+    private Boolean active;
+    private Player instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.active = false;
+        this.instance = new Player(30, 0);
+    }
+
+    void setStatus(Boolean status)
+    {
+        this.active = status;
+    }
+}
