eef14cc04b5dccc22564916dbd3d250e33fd75e6
Xanfire001 <alex.weichbrodt33546@gmail.com>
2020-05-25 14:52:43 +0200

    Revert "Added Card Drawing, Changed Card Deck Hierarchy"
    This reverts commit 9a7edbec60945416c4b23f8278438f9adc10df94.

--- CardGame/Assets/Scripts/Objects/GameDeck.cs
+++ CardGame/Assets/Scripts/Objects/GameDeck.cs
@@ -2,7 +2,6 @@
 using Assets.Scripts.Entitys;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class GameDeck : MonoBehaviour
@@ -31,18 +30,5 @@ public class GameDeck : MonoBehaviour
     void Update()
     {
 
-    }
-
-    private void OnMouseDown()
-    {
-        var temp = this.transform.GetChild(0);
-        if (this.transform.parent.Find("HandCards").childCount >= 3)
-        {
-            Destroy(temp.gameObject);
-        }
-        else
-        {
-            temp.transform.parent = this.transform.parent.Find("HandCards");
-        }
     }
 }
